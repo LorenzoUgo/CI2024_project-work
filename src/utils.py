e920d5e4bd88b0b229bbfe66b614c3be6d68ea23
Lorenzo Ugoccioni <s315734@studenti.polito.it>
2025-01-30 01:12:00 +0100

    NEW: GA class initialization, deploy and readable functiono for individual

--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,11 @@
 # All the supporting function needed for my algorithm.
 # ##
 import numpy as np
+import itertools
 from typing import Callable
 import numbers
 import warnings
+import random
 from draw import draw
 
 numpy_funct = {
@@ -16,10 +18,12 @@ numpy_funct = {
     "rounding":         [np.ceil, np.round, np.trunc],           #SOLO come supporto post-generazione per ridurre le cifre decimali dei valori
 }
 
+numpy_cost = {np.e, np.pi, np.euler_gamma, np.finfo(float).eps}
+
 class Individual:
-    def __init__(self, function: 'Node'):
+    def __init__(self, f: 'Node'):
         self.MSE = self.__MSE__
-        self.function = function
+        self.SymRegTree = f
         self.fitness = self.__fitness__()
 
     def __MSE__(self, x, y):
@@ -29,38 +33,50 @@ class Individual:
         ''' MSE + f_length '''
         ...
 
+    def gene_mutation(self, new):
+        ''' What do I want to mutate? '''
+        if callable(new):
+            self.SymRegTree.replace_function(new)
+        elif isinstance(new, numbers.Number):
+            self.SymRegTree.replace_const(new)
+        elif isinstance(new, str):
+            self.SymRegTree.replace_var(new)
 
 class Node:
     _value: int
-    #_func: function
     _name: str
     _successor: tuple['Node']
 
     def __init__(self, node=None, successors=None):
+
         if callable(node):
             ''' The node is a function --> Must have some successors --> Not a leaf '''
             def _f(*_args, **_kwargs):
                 return node(*_args)
 
-            self._func = _f
-            self._successor = tuple(successors)
+            self._value = _f
             self._leaf = False
             self._name = node.__name__
 
         elif isinstance(node, numbers.Number):
-            ''' The node is a number --> Must be a leaf '''
+            ''' The node is a number --> A leaf '''
             self._value = node
-            self._successor = tuple()
             self._leaf = True
             self._name = str(node)
 
         elif isinstance(node, str):
-            ''' The node is a variable --> Must be a leaf '''
+            ''' The node is a variable --> A leaf '''
             self._value = node
-            self._successor = tuple()
             self._leaf = True
             self._name = node
-
+        
+        self._successor = tuple()
+        if successors is not None:
+            if not isinstance(successors, list):
+                successors = list(successors)
+            for item in list(successors):
+                self.add_successor(item)
+        
     def __str__(self):
         num_child = sum(x is not None for x in self._successor)
         if num_child == 2:
@@ -70,6 +86,57 @@ class Node:
         
         return self._name
 
+    def __apply_f__(self, var_value):
+        if self._leaf:
+            if isinstance(self._value, str):
+                return var_value
+            else:
+                return self._value
+        
+        return self._value(*[next.__apply_f__(var_value) for next in self._successor])
+    
+    def apply(self, var_value):
+        return self.__apply_f__(var_value)
+    
+    def add_successor(self, n:'Node'):
+        if len(self._successor) == 2 :
+            raise IndexError("Can't have more than 2 successor")
+        
+        self._successor = tuple(list(self._successor).append(n))
+    
+    def get_level(self):
+        if self._leaf:
+            return 0
+        
+        return 1 + max(child.get_level() for child in self._successor)
+
+    def replace_function(self, new_f):
+        if not (0 <= index < len(self._successor)):
+            raise IndexError("Index out of range for successor tuple")
+        
+        new_successors = tuple(
+            new_node if i == index else child for i, child in enumerate(self._successor)
+        )
+        return 
+
+    def replace_const(self, new_c):
+        if not (0 <= index < len(self._successor)):
+            raise IndexError("Index out of range for successor tuple")
+        
+        new_successors = tuple(
+            new_node if i == index else child for i, child in enumerate(self._successor)
+        )
+        return 
+
+    def replace_var(self, new_v):
+        if not (0 <= index < len(self._successor)):
+            raise IndexError("Index out of range for successor tuple")
+        
+        new_successors = tuple(
+            new_node if i == index else child for i, child in enumerate(self._successor)
+        )
+        return 
+
     def draw(self):
         try:
             return draw(self)
@@ -77,14 +144,49 @@ class Node:
             warnings.warn(f"Drawing not available ({msg})", UserWarning, 2)
             return None
 
-    def __apply_f__(self, var_value):
-        if not len(self._successor):
-            if isinstance(self._value, str):
-                return var_value
+
+class Genetic_Algorithm:
+    _population_size: int
+    _offsprings: int
+    _num_islands: int
+    _population: list[Individual]
+    _num_generations: int
+    _num_eras: int
+    _num_variables: int
+
+    def __init__(self, pop_size, off_num, num_isl, num_gen, num_eras, num_var):
+        self._num_islands = num_isl
+        self._offsprings = off_num
+        self._population_size = pop_size
+        self._num_generations = num_gen
+        self._num_eras = num_eras
+        self._num_variables = num_var
+        self._population = self.__random_init__()
+
+
+    def __random_init__(self):
+        population = []
+
+        var_list = [f"x{i}" for i in range(self._num_variables)]
+
+        for i in range(self._population_size):
+            if self._num_variables == 1:
+                f = Node(var_list[0])
+                ind = Individual(f)
+                population.append(ind)
             else:
-                return self._value
-        
-        return self._func(*[next.__apply_f__(var_value) for next in self._successor])
+                j = 0
+                operand = []
+                while j < range(self._num_variables-1):
+                    op = random.choice(list(itertools.chain(*numpy_funct.values())))
+                    operand.append(op)
+                    if op.nin > 1:
+                        j += 1
+
+                
+        return population
+
+
 
 
 def function_cod(num: int) -> list[str]:
@@ -117,6 +219,7 @@ def train(function: list, x: list, y:list) -> float:
     return MSE
 
 ###
+  # Capire se una fuzione e uni o bi arg è importante quando si fanno modifiche o tagli !!
   # Longer function --> Penalty
   # Genetic Algo / Evolutionary Strategy / ... 
   # Graph representation of function
